80690a08ee6a93ddee8053651044b27de285b460
Erlend <elandroe@icloud.com>
2017-05-25 09:15:02 +0200

    setup basic homepage

--- src/WasteTrader/Api/MainHandlers.cs
+++ src/WasteTrader/Api/MainHandlers.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Starcounter;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WasteTrader.ViewModels;
 
 namespace WasteTrader.Api
 {
@@ -10,7 +12,34 @@ namespace WasteTrader.Api
     {
         public void Register()
         {
+            Application.Current.Use(new HtmlFromJsonProvider());
+            Application.Current.Use(new PartialToStandaloneHtmlProvider());
 
+            Handle.GET("/WasteTrader", () =>
+            {
+                var master = GetMasterPage();
+
+                master.CurrentPage = new HomePage();
+
+                return master;
+            });
+        }
+
+        private MasterPage GetMasterPage()
+        {
+            if (Session.Current == null)
+            {
+                Session.Current = new Session(SessionOptions.PatchVersioning);
+            }
+
+            MasterPage master = Session.Current.Data as MasterPage;
+
+            if (master == null)
+            {
+                master = new MasterPage() { Session = Session.Current };
+            }
+
+            return master;
         }
     }
 }

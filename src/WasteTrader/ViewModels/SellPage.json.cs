05a37f7edc590c2cc7fb95f9dd5fe7142cd93fa7
Erlend <elandroe@icloud.com>
2017-05-25 12:25:33 +0200

    make it possible to add waste to the database

--- src/WasteTrader/ViewModels/SellPage.json.cs
+++ src/WasteTrader/ViewModels/SellPage.json.cs
@@ -1,8 +1,20 @@
 using Starcounter;
+using WasteTrader.Database;
 
 namespace WasteTrader.ViewModels
 {
     partial class SellPage : Json
     {
+        void Handle(Input.SubmitTrigger action)
+        {
+            Db.Transact(() =>
+            {
+                new Waste
+                {
+                    Description = this.Waste.Description,
+                    Quantity = (double) this.Waste.Quantity
+                };
+            });
+        }
     }
 }

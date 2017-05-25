fc180823bfd7caac57593cc232b268024b8893cc
Erlend <elandroe@icloud.com>
2017-05-25 12:08:16 +0200

    simplify waste model

--- src/WasteTrader/Database/Waste.cs
+++ src/WasteTrader/Database/Waste.cs
@@ -2,18 +2,11 @@
 
 namespace WasteTrader.Database
 {
-    public enum Unit
-    {
-        Kg,
-        m2,
-        m3,
-    }
-
     [Database]
     public class Waste
     {
         public string Description { get; set; }
         public double Quantity { get; set; }
-        public Unit Unit { get; set; }
+        public string Unit { get; set; }
     }
 }

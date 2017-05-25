9eb7d7208e5abb6ff03fa3f75620bbbccbc6503d
Erlend <elandroe@icloud.com>
2017-05-25 10:43:29 +0200

    add basic styling to tabs

--- src/WasteTrader/Api/MainHandlers.cs
+++ src/WasteTrader/Api/MainHandlers.cs
@@ -12,8 +12,29 @@ namespace WasteTrader.Api
     {
         public void Register()
         {
+            var html = @"<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=""utf-8"">
+    <title>{0}</title>
+    <script src=""/sys/webcomponentsjs/webcomponents.min.js""></script>
+    <link rel=""import"" href=""/sys/polymer/polymer.html"">
+    <link rel=""import"" href=""/sys/starcounter.html"">
+    <link rel=""import"" href=""/sys/starcounter-include/starcounter-include.html"">
+    <link rel=""import"" href=""/sys/starcounter-debug-aid/src/starcounter-debug-aid.html"">
+    <link rel=""import"" href=""/sys/bootstrap.html"">
+</head>
+<body>
+    <template is=""dom-bind"" id=""puppet-root"">
+        <template is=""imported-template"" content$=""{{{{model.Html}}}}"" model=""{{{{model}}}}""></template>
+    </template>
+    <puppet-client ref=""puppet-root"" remote-url=""{1}""></puppet-client>
+    <starcounter-debug-aid></starcounter-debug-aid>
+</body>
+</html>";
+
             Application.Current.Use(new HtmlFromJsonProvider());
-            Application.Current.Use(new PartialToStandaloneHtmlProvider());
+            Application.Current.Use(new PartialToStandaloneHtmlProvider(html));
 
             Handle.GET("/WasteTrader", () =>
             {

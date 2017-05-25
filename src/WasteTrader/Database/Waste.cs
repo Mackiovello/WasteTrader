e44cae64c695302cb5bdce24c01676bba9846a85
Erlend <elandroe@icloud.com>
2017-05-25 12:07:20 +0200

    add waste class

---
+++ src/WasteTrader/Database/Waste.cs
@@ -0,0 +1,19 @@
+﻿using Starcounter;
+
+namespace WasteTrader.Database
+{
+    public enum Unit
+    {
+        Kg,
+        m2,
+        m3,
+    }
+
+    [Database]
+    public class Waste
+    {
+        public string Description { get; set; }
+        public double Quantity { get; set; }
+        public Unit Unit { get; set; }
+    }
+}

a90730a3b8fd2782e5f84e3f5fb878e1c87bcf68
philipborg <philip.borgstrom@gmail.com>
2017-06-02 15:49:22 +0200

    Implemented get function for Location.Waste

--- src/WasteTrader/Database/Location.cs
+++ src/WasteTrader/Database/Location.cs
@@ -1,5 +1,6 @@
 ﻿using Starcounter;
 using WasteTrader.MathUtils;
+using System.Linq;
 
 namespace WasteTrader.Database
 {
@@ -8,7 +9,17 @@ namespace WasteTrader.Database
     {
         public double LongitudeDD { get; set; }
         public double LatitudeDD { get; set; }
-        public IWaste Waste { get; }
+        public IWaste Waste
+        {
+            get
+            {
+                var buyWaste = Db.SQL<BuyWaste>("SELECT i FROM WasteTrader.BuyWaste i WHERE i.Location = ?", this).FirstOrDefault();
+                if (buyWaste != null) return buyWaste;
+                var sellWaste = Db.SQL<SellWaste>("SELECT i FROM WasteTrader.SellWaste i WHERE i.Location = ?", this).FirstOrDefault();
+                if (sellWaste != null) return sellWaste;
+                else throw new System.Exception("Location " + this.GetObjectNo() + " has no Waste bound to it.");
+            }
+        }
         public double LongitudeRadians => GeographyMath.DegreesToRadians(LongitudeDD);
         public double LatitudeRadians => GeographyMath.DegreesToRadians(LatitudeDD);
     }

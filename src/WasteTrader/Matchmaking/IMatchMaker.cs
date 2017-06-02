0e04aa7109b7bc1891437c34f55a0fa4f72af850
philipborg <philip.borgstrom@gmail.com>
2017-06-02 16:01:27 +0200

    Added som C#doc comments

--- src/WasteTrader/Matchmaking/IMatchMaker.cs
+++ src/WasteTrader/Matchmaking/IMatchMaker.cs
@@ -5,8 +5,24 @@ namespace WasteTrader.Matchmaking
 {
     public interface IMatchMaker
     {
+        /// <summary>
+        /// Searches for BuyWaste based on parameters
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <returns>Matched BuyWaste</returns>
         BuyWaste[] MatchSeller(IMatchParameters parameters);
+        /// <summary>
+        /// Searches for SellWaste based on parameters
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <returns>Matched SellWaste</returns>
         SellWaste[] MatchBuyer(IMatchParameters parameters);
+        /// <summary>
+        /// Searches through a set of IWaste based on parameters
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <param name="searchspace"></param>
+        /// <returns>Matched IWaste</returns>
         IWaste[] Match(IMatchParameters parameters, IEnumerable<IWaste> searchspace);
     }
 }
--- src/WasteTrader/Measurements/IMeasurement.cs
+++ src/WasteTrader/Measurements/IMeasurement.cs
@@ -7,16 +7,33 @@ namespace WasteTrader.Measurements
 {
     public interface IMeasurement<T> : IComparable, IEquatable<T>, IEqualityComparer<T>
     {
+        /// <summary>
+        /// All generally accepted units for a specific measurement.
+        /// </summary>
         IImmutableDictionary<sbyte,Unit> Symbols { get; }
 
+        /// <summary>
+        /// The value in the baseunit
+        /// </summary>
         long Value { get; }
 
+        /// <summary>
+        /// The value in specified unit
+        /// </summary>
+        /// <param name="unit"></param>
+        /// <returns></returns>
         long CalcValue(Unit unit);
-
+        /// <summary>
+        /// The power to use, i.e. 10^x
+        /// </summary>
         sbyte UnitMetricPrefixPower { get; set; }
-
+        /// <summary>
+        /// The value before 10^x
+        /// </summary>
         long Quantity { get; set; }
-
+        /// <summary>
+        /// Raises, if possible and safe, the UnitMetricPrefixPower to a higher value while lowering Quantity.
+        /// </summary>
         [MethodImpl(MethodImplOptions.Synchronized)]
         void ConvertOptimal();
     }

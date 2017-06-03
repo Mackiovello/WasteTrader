682e9bdae107abebea634858b83ca925a05325a9
Erlend <elandroe@icloud.com>
2017-06-03 11:56:03 +0200

    include material components js and css

--- src/WasteTrader/Api/MainHandlers.cs
+++ src/WasteTrader/Api/MainHandlers.cs
@@ -22,6 +22,7 @@ namespace WasteTrader.Api
     <link rel=""import"" href=""/sys/starcounter.html"">
     <link rel=""import"" href=""/sys/starcounter-include/starcounter-include.html"">
     <link rel=""import"" href=""/sys/starcounter-debug-aid/src/starcounter-debug-aid.html"">
+    <link rel=""stylesheet"" href=""/sys/material-components-web.min.css"">
 </head>
 <body>
     <template is=""dom-bind"" id=""puppet-root"">
@@ -29,6 +30,7 @@ namespace WasteTrader.Api
     </template>
     <puppet-client ref=""puppet-root"" remote-url=""{1}""></puppet-client>
     <starcounter-debug-aid></starcounter-debug-aid>
+    <script src=""/sys/material-components-web.min.js""></script>
  </body>
 </html>";
 

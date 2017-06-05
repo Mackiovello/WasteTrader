777af034f3125029a37ac59c36e61162200ebcd8
philipborg <philip.borgstrom@gmail.com>
2017-06-05 14:54:30 +0200

    Added PriceSorter and moved IMatchSorter

---
+++ src/WasteTrader/Matchmaking/Sorters/PriceSorter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WasteTrader.Database;
+
+namespace WasteTrader.Matchmaking.Sorters
+{
+    public class PriceSorter : GenericSorter
+    {
+        public PriceSorter(bool descendingOrder)
+        {
+            this.DescendingOrder = descendingOrder;
+        }
+
+        protected override IComparable Valuer(Waste waste)
+        {
+            return waste.Price;
+        }
+    }
+}

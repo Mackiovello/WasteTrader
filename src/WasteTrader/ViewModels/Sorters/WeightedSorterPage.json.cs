43af1b0764c1beb5c36bbecbe540447103d170e7
philipborg <philip.borgstrom@gmail.com>
2017-06-07 14:30:18 +0200

    Added WeightedSorter view

--- src/WasteTrader/ViewModels/Sorters/WeightedSorterPage.json.cs
+++ src/WasteTrader/ViewModels/Sorters/WeightedSorterPage.json.cs
@@ -11,5 +11,35 @@ namespace WasteTrader.ViewModels.Sorters
             var sorters = page.Sorters.AsParallel().ToDictionary(s => (IMatchSorter) s.Sorter, s => (double) s.Weight);
             return new WeightedSorter(page.DescendingOrder, sorters);
         }
+
+        void Handle(Input.AddSorter action)
+        {
+            SortersElementJson newSorter = new SortersElementJson();
+            switch (action.Value)
+            {
+                case "DateSorter":
+                    newSorter.Sorter = new DateSorterPage();
+                    newSorter.Name = "Date sorter";
+                    break;
+                case "DistanceSorter":
+                    newSorter.Sorter = new DistanceSorterPage();
+                    newSorter.Name = "Distance Sorter";
+                    break;
+                case "PPUSorter":
+                    newSorter.Sorter = new PricePerUnitSorterPage();
+                    newSorter.Name = "Price per unit Sorter";
+                    break;
+                case "PriceSorter":
+                    newSorter.Sorter = new PriceSorterPage();
+                    newSorter.Name = "Price Sorter";
+                    break;
+                case "QuantitySorter":
+                    newSorter.Sorter = new QuantitySorterPage();
+                    newSorter.Name = "Quantity Sorter";
+                    break;
+                default: return;
+            }
+            this.Sorters.Add(newSorter);
+        }
     }
 }

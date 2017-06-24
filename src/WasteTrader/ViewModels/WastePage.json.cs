a543e2b73d4b5d6bcef06949cdda44563fb6db03
Erlend <elandroe@icloud.com>
2017-06-24 15:14:56 +0200

    make it possible to delete waste

--- src/WasteTrader/ViewModels/WastePage.json.cs
+++ src/WasteTrader/ViewModels/WastePage.json.cs
@@ -5,6 +5,13 @@ namespace WasteTrader.ViewModels
 {
     partial class WastePage : Json, IBound<Waste>
     {
-        public string FormattedEntryTime => Data.EntryTime.Date.ToString("d");
+        public string FormattedEntryTime => Data?.EntryTime.Date.ToString("d");
+
+        void Handle(Input.DeleteTrigger action)
+        {
+            this.RedirectUrl = $"/Waste2Value/user/{this.User.Username}";
+            var waste = Db.FromId<Waste>(this.Key);
+            Db.Transact(() => waste.Delete());
+        }
     }
 }

f91bd94b94ed8fbe416650f4f969b63910e50103
Erlend <elandroe@icloud.com>
2017-06-24 10:51:38 +0200

    refactor client class

--- src/WasteTrader/Database/Client.cs
+++ src/WasteTrader/Database/Client.cs
@@ -8,15 +8,33 @@ namespace WasteTrader.Database
     [Database]
     public class Client
     {
+        private static readonly string SELECT_CLIENT_WHERE_USER = 
+            $"SELECT c FROM {typeof(Client)} c WHERE c.{nameof(User)} = ?";
+
+        private static readonly string SELECT_USER_WHERE_USERNAME = 
+            $"SELECT u FROM {typeof(SystemUser)} u WHERE u.{nameof(SystemUser.Username)} = ?";
+
+        private static readonly string SELECT_EMAILADDRESSRELATION_WHERE_SOMEBODY = 
+            $"SELECT r FROM {typeof(EmailAddressRelation)} r WHERE r.{nameof(EmailAddressRelation.Somebody)} = ?";
+
+        private static readonly string SELECT_SELLWASTE_WHERE_USER =
+            $"SELECT w FROM {typeof(SellWaste)} w WHERE w.{nameof(SellWaste.User)} = ?";
+
+        private static readonly string SELECT_BUYWASTE_WHERE_USER =
+            $"SELECT w FROM {typeof(BuyWaste)} w WHERE w.{nameof(BuyWaste.User)} = ?";
+
+        private static readonly string SELECT_WASTE_WHERE_USER =
+            $"SELECT w FROM {typeof(Waste)} w WHERE w.{nameof(Waste.User)} = ?";
+
         public Client(SystemUser sysUser)
         {
-            User = sysUser;
+            this.User = sysUser;
         }
 
         public static Client GetClient(SystemUser systemUser)
         {
             if (systemUser == null) return null;
-            Client client = Db.SQL<Client>($"SELECT c FROM {typeof(Client)} c WHERE c.{nameof(User)} = ?", systemUser).FirstOrDefault();
+            Client client = Db.SQL<Client>(SELECT_CLIENT_WHERE_USER, systemUser).FirstOrDefault();
             if (client == null)
             {
                 Db.Transact(() => client = new Client(systemUser));
@@ -26,7 +44,7 @@ namespace WasteTrader.Database
 
         public static Client GetClientFromUsername(string username)
         {
-            Client user = Db.SQL<SystemUser>($"SELECT u FROM {typeof(SystemUser)} u WHERE u.{nameof(SystemUser.Username)} = ?", username).FirstOrDefault();
+            Client user = Db.SQL<SystemUser>(SELECT_USER_WHERE_USERNAME, username).FirstOrDefault();
             return user;
         }
 
@@ -40,15 +58,15 @@ namespace WasteTrader.Database
         {
             get
             {
-                EmailAddressRelation relation = Db.SQL<EmailAddressRelation>($"SELECT r FROM {typeof(EmailAddressRelation)} r WHERE r.{nameof(EmailAddressRelation.Somebody)} = ?", User).First;
+                var relation = Db.SQL<EmailAddressRelation>(SELECT_EMAILADDRESSRELATION_WHERE_SOMEBODY, User).FirstOrDefault();
                 return relation.EmailAddress.EMail;
             }
         }
-        public string Username { get => User.Username; }
-        public string Name { get => User.Name; }
-        public string Description { get => User.Description; }
-        public IEnumerable<SellWaste> SellWastes => Db.SQL<SellWaste>($"SELECT w FROM {typeof(SellWaste)} w WHERE w.{nameof(SellWaste.User)} = ?", this);
-        public IEnumerable<BuyWaste> BuyWastes => Db.SQL<BuyWaste>($"SELECT w FROM {typeof(BuyWaste)} w WHERE w.{nameof(BuyWaste.User)} = ?", this);
-        public IEnumerable<Waste> WasteEntries => Db.SQL<Waste>($"SELECT w FROM {typeof(Waste)} w WHERE w.{nameof(Waste.User)} = ?", this);
+        public string Username => User.Username;
+        public string Name => User.Name;
+        public string Description => User.Description;
+        public IEnumerable<SellWaste> SellWastes => Db.SQL<SellWaste>(SELECT_SELLWASTE_WHERE_USER, this);
+        public IEnumerable<BuyWaste> BuyWastes => Db.SQL<BuyWaste>(SELECT_BUYWASTE_WHERE_USER, this);
+        public IEnumerable<Waste> WasteEntries => Db.SQL<Waste>(SELECT_WASTE_WHERE_USER, this);
     }
 }

25cfceb479a840990ca58dc07f68c5ce11dd117a
Erlend <elandroe@icloud.com>
2017-06-25 18:39:17 +0200

    create basic waste matching class

---
+++ src/WasteTrader/Helpers/WasteMatching.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WasteTrader.Database;
+
+namespace WasteTrader.Helpers
+{
+    public class WasteMatching
+    {
+        private Waste WasteToMatch { get; set; }
+        private IEnumerable<Waste> ToMatchAgainst { get; set; }
+        public IEnumerable<Waste> Matches { get; private set; }
+
+        public WasteMatching(Waste wasteToMatch, IEnumerable<Waste> toMatchAgainst)
+        {
+            this.WasteToMatch = wasteToMatch ?? throw new ArgumentNullException("wasteToMatch");
+            this.ToMatchAgainst = toMatchAgainst ?? throw new ArgumentNullException("toMatchAgainst");
+        }
+
+        public WasteMatching Match()
+        {
+            string titleToMatchAgainst = WasteToMatch.Title;
+            List<Waste> matches = new List<Waste>();
+
+            foreach (var waste in ToMatchAgainst)
+            {
+                if (waste.Title == titleToMatchAgainst)
+                    matches.Add(waste);
+            }
+
+            Matches = matches;
+
+            return this;
+        }
+    }
+}

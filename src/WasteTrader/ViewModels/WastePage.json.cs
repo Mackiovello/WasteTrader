54b9b99664d6616da8e71fd0edb7b316ea2c8b3b
Erlend <elandroe@icloud.com>
2017-07-01 11:19:40 +0200

    fix deletion of waste

--- src/WasteTrader/ViewModels/WastePage.json.cs
+++ src/WasteTrader/ViewModels/WastePage.json.cs
@@ -7,6 +7,12 @@ namespace WasteTrader.ViewModels
     {
         public string FormattedEntryTime => Data?.EntryTime.Date.ToString("d");
 
+        void Handle(Input.DeleteTrigger action)
+        {
+            this.RedirectUrl = $"/Waste2Value/user/{this.User.Username}";
+            Db.Transact(() => this.Data.Active = false);
+        }
+
         [WasteEntry_json.User]
         partial class WasteUser : Json, IBound<Client>
         {

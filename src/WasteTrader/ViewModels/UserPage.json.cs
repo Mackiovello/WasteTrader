50d9bf5801e206dcda04689afbf6151b432218cc
Erlend <elandroe@icloud.com>
2017-07-02 18:19:36 +0200

    display a note if there are no entries on 'Min sida', for #56

--- src/WasteTrader/ViewModels/UserPage.json.cs
+++ src/WasteTrader/ViewModels/UserPage.json.cs
@@ -7,6 +7,18 @@ namespace WasteTrader.ViewModels
 {
     partial class UserPage : Json, IBound<Database.Client>
     {
+        public bool NoWaste => this.WasteToDisplay.Count() == 0;
+        public string NoWasteMessage
+        {
+            get
+            {
+                if (this.WasteToDisplay == this.ActiveWaste)
+                    return "Du har inga aktiva annonser just nu";
+                else
+                    return "Du har inga inaktiva annonser just nu";
+            }
+        }
+
         protected override void OnData()
         {
             this.WasteToDisplay = this.ActiveWaste;
